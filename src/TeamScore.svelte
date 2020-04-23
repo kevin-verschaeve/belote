6a32de198e570aef668ad9e34d718bcb7b56e321
Kevin Verschaeve <kevin.verschaeve@live.fr>
2020-04-23 22:24:29 +0200

    remove useless if

--- src/TeamScore.svelte
+++ src/TeamScore.svelte
@@ -7,16 +7,14 @@
 
 <div class="plis-wrapper">
     <h3 class="text">{players.filter((p) => p.team == team).map((p) => p.name).join(' / ')}</h3>
-    {#if plis.length}
-        {#each plis as pli}
-            <div class="pli">
-                {#each JSON.parse(pli) as card}
-                    <Card {card}/>
-                {/each}
-            </div>
-        {/each}
+    {#each plis as pli}
+        <div class="pli">
+            {#each JSON.parse(pli) as card}
+                <Card {card}/>
+            {/each}
+        </div>
     {:else}
         <p class="capot text">Capot dans vos gueules !</p>
         <img src="https://media.giphy.com/media/UTYz3M8lcTvqaVbSo9/source.gif" width="400"/>
-    {/if}
+    {/each}
 </div>
