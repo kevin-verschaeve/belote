4054c72775e016b06ca7e928f307ca1e5beb47ce
Kevin Verschaeve <kevin.verschaeve@live.fr>
2020-12-01 00:38:07 +0100

    Add win animation and modal

--- src/GameCount.svelte
+++ src/GameCount.svelte
@@ -1,13 +1,38 @@
 <script>
-    import { getContext } from 'svelte';
+    import { getContext, afterUpdate } from 'svelte';
     import { createGame, dealPreGame } from './GameManager.js';
     import { getOneCard, cutDeck } from './DeckManager.js';
     import { calculateRoundScores } from './PliManager.js';
+    import confetti from 'canvas-confetti';
     import TeamScore from './TeamScore.svelte';
 
     export let game;
     export let gameRef;
     export let players;
+
+    afterUpdate(() => {
+        if (newGlobalScore.NS > 1000 || newGlobalScore.EW > 1000) {
+            M.AutoInit();
+            M.Modal.getInstance(document.getElementById('modal-win')).open();
+            const duration = 8 * 1000;
+            const animationEnd = Date.now() + duration;
+            const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 1005 };
+            const randomInRange = (min, max) => Math.random() * (max - min) + min;
+
+            const interval = setInterval(() => {
+                const timeLeft = animationEnd - Date.now();
+
+                if (timeLeft <= 0) {
+                    return clearInterval(interval);
+                }
+
+                const particleCount = 50 * (timeLeft / duration);
+                // since particles fall down, start a bit higher than random
+                confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+                confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+            }, 250);
+        }
+    });
 
     const db = getContext('firebase').firestore();
 
@@ -60,3 +85,17 @@
         <TeamScore plis={game.EW} players={players.filter((p) => p.team == 'EW')} points={game.score.EW} realPoints={scores.EW} globalScore={newGlobalScore.EW}/>
     </div>
 </div>
+
+{#if newGlobalScore.NS > 1000 || newGlobalScore.EW > 1000}
+<div id="modal-win" class="modal center-align">
+    <div class="modal-content relative">
+        <h4>C'est gagné !</h4>
+        <h5>
+            Vainqueurs :
+            {players.filter((p) => p.team == (newGlobalScore.NS > newGlobalScore.EW ? 'NS' : 'EW')).map((p) => p.name).join(' et ')}
+        </h5>
+        <img src="dora.png" alt="Dora, c'est gagné !"/>
+        <button class="btn modal-close waves-effect btn-flat">&times;</button>
+    </div>
+</div>
+{/if}
